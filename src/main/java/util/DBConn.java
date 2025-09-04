efa5475ebcd2b4950b6c2518c002ca3dfc01a8fe
Lâm Mai <lamssdd910@gmail.com>
2025-09-04 15:53:30 +0700

    Nop bai tap, em lo xoa a

--- src/main/java/util/DBConn.java
+++ src/main/java/util/DBConn.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 
 
 public class DBConn {
-	private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=ltweb;encrypt=false;trustServerCertificate=true";
-    private static final String USER = "sa";
-    private static final String PASS = "123456";
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/web";
+    private static final String USER = "root";
+    private static final String PASS = "Lammaisd910!";
 
     public static Connection getConnection() throws Exception {
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
